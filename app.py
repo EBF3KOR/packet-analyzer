6114fc602156ac7f923e44107298c7f61f1c5da3
EBF3KOR <ebf3kor@bosch.com>
2024-11-26 13:50:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from kamene.all import rdpcap, IP, TCP, UDP, Ether, IPv6
+from scapy import rdpcap, IP, TCP, UDP, Ether, IPv6
 import pandas as pd
 import numpy as np
 import pytz
